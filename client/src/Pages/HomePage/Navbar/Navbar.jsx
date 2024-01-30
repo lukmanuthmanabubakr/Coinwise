2b3e5ab69ece4017f8c1883b73a35a6e9cb304fd
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-30 14:55:45 +0100

    greate job

--- client/src/Pages/HomePage/Navbar/Navbar.jsx
+++ client/src/Pages/HomePage/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 import logo from "../../../Asset/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='navbarComponent'>
+    <nav className="navbarComponent">
       <div>
-        <img src={logo} alt='logos'/>
-        
+        <NavLink>
+          <img src={logo} alt="logos" />
+          <p>coin<span>Wise</span></p>
+        </NavLink>
       </div>
       <div></div>
       <div></div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
