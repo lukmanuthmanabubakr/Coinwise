e146320375ab6c93fc640de5c123c7cf610cef71
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-31 12:44:40 +0100

    interesting

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+// import useLocalStorage from "use-local-storage"
+// import Navbar from './Pages/HomePage/Navbar/Navbar';
+
+// function App() {
+
+//   const [myTheme, setMyTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+//   const switchTheme = () => {
+//     const newTheme = myTheme === "light" ? "dark" : "light";
+//     setMyTheme(newTheme);
+//   };
+//   return (
+//     <div className="app" data-theme={myTheme}>
+//         <Navbar switchTheme={switchTheme}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from 'react';
 import './App.css';
 import useLocalStorage from "use-local-storage"
 import Navbar from './Pages/HomePage/Navbar/Navbar';
 
 function App() {
-  const [myTheme, setMyTheme] = useLocalStorage("theme" ? "dark" : "light");
+  const [isDarkMode, setIsDarkMode] = useLocalStorage("theme", false);
 
   const switchTheme = () => {
-    const newTheme = myTheme === "light" ? "dark" : "light";
-    setMyTheme(newTheme);
+    setIsDarkMode(prevMode => !prevMode);
   };
+
   return (
-    <div className="app" data-theme={myTheme}>
-        <Navbar switchTheme={switchTheme}/>
+    <div className="app" data-theme={isDarkMode ? 'dark' : 'light'}>
+      <Navbar switchTheme={switchTheme} isDarkMode={isDarkMode} />
     </div>
   );
 }
--- client/src/Pages/HomePage/Navbar/Navbar.jsx
+++ client/src/Pages/HomePage/Navbar/Navbar.jsx
@@ -1,18 +1,70 @@
-import React from "react";
+// import React, { useState } from "react";
+// import logo from "../../../Asset/logo.png";
+// import { NavLink } from "react-router-dom";
+// import { FiSun } from "react-icons/fi";
+// import { FiMoon } from "react-icons/fi";
+// import "./Navbar.css"
+
+
+// const Navbar = ({ switchTheme }) => {
+//   return (
+//     <nav className="navbarComponent">
+//       <div>
+//         <NavLink>
+//           <img src={logo} alt="logos" />
+//           <p>coin<span>Wise</span></p>
+//         </NavLink>
+//       </div>
+//       <div>
+//         <NavLink>Dashboard</NavLink>
+//         <NavLink>Libary</NavLink>
+//         <NavLink>Company</NavLink>
+//         <NavLink>About</NavLink>
+//         <NavLink>Contact-Us</NavLink>
+//       </div>
+//       <div>
+//         <button onClick={switchTheme}><FiSun /></button>
+//         <button><FiMoon /></button>
+//         <NavLink>Login</NavLink>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+import React, { useState } from "react";
 import logo from "../../../Asset/logo.png";
 import { NavLink } from "react-router-dom";
+import { FiSun, FiMoon } from "react-icons/fi";
+import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ switchTheme, isDarkMode }) => {
   return (
     <nav className="navbarComponent">
       <div>
-        <NavLink>
-          <img src={logo} alt="logos" />
-          <p>coin<span>Wise</span></p>
+        <NavLink to="/">
+          <img src={logo} alt="logo" />
+          <p>
+            coin<span>Wise</span>
+          </p>
         </NavLink>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/library">Library</NavLink>
+        <NavLink to="/company">Company</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
+      </div>
+      <div>
+        <button onClick={switchTheme}>
+          {isDarkMode ? <FiSun /> : <FiMoon />}
+        </button>
+        <NavLink to="/login">Login</NavLink>
+      </div>
     </nav>
   );
 };
