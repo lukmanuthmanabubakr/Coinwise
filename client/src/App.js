5540e01a8a057a8f2936a3dbf89e4e69bae070a7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-01-31 15:01:31 +0100

    omo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import useLocalStorage from "use-local-storage"
 import Navbar from './Pages/HomePage/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage';
+import About from './Pages/About/About';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useLocalStorage("theme", false);
@@ -13,8 +16,12 @@ function App() {
   return (
     <div className="app" data-theme={isDarkMode ? 'dark' : 'light'}>
       <Navbar switchTheme={switchTheme} isDarkMode={isDarkMode} />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
