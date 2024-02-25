d749bed5205d2a91f01656fec76841b0dcca2356
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-25 11:30:35 +0100

    chlie

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import FirstComponent from './FirstComponent/FirstComponent';
 
 const HomePage = () => {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <FirstComponent />
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
