5c78c39d02809029a7f150f2cd66c7340076b033
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-25 13:05:40 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -13,6 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start 
+// If you 
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
