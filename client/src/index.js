80827420966bf8d6b177017fb24a2deaa212219d
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-25 13:05:25 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -13,6 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app
+// If you want to start measuring performan
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
